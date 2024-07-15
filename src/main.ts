c83fb5b5cfd9f738ca44fc1176a0bb4bc7bf5553
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-07-15 14:44:47 +0200

    fixed main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,7 @@ const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use()
-  
+
   await app.listen(3000);
 }
 
