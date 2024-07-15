993c7405dc9e068523298e67106245a7f9877f10
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-07-15 12:23:46 +0200

    modified file for end to end testing

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUserService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
+
 
   beforeEach(async () => {
+    fakeUserService = {
+      findOne: (id: number) => {
+        return Promise.resolve({id, email: 'aoasoas@gmail.com', password: 'jksjdlka'} as User)
+      },
+      find: (email: string) => {
+        return Promise.resolve([{id: 1, email, password: 'askhas'} as User])
+      },
+      // remove: () => {},
+      // update: () => {},
+
+    };
+    fakeAuthService = {
+      // signup: () => {},
+      // signin: () => {},
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: []
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -15,4 +37,6 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+
 });
