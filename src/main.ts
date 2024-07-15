9e2af4e9fd39550a5df81290d508bf8a7943cf87
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-07-15 14:28:11 +0200

    commented old version of main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,25 @@ const cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use()
-
   
-  /*! APPLICATO IN APP.MODULE PER RISOLVERE PROBLEMA TEST E2E */
-  /* app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    })
-  ) */
-
-
   await app.listen(3000);
 }
 
 bootstrap();
+
+
+
+/* async function bootstrap() {
+  const app = await NestFactory.create(AppModule,);
+  app.use(cookieSession({
+    // stringa casuale per criptare le info memorizzate all'interno dei cookie
+    keys: ['Canestro78']
+  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true
+    })
+  )
+  // app.useLogger(app.get(Logger));
+  await app.listen(3000);
+} */
