56e7bf0dc3c54d899da1e3a5700ba753ecdc237f
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-07-16 12:30:59 +0200

    added relationships between user and reports II

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, UseGuards, ExecutionContext } from '@nestjs/com
 import { CreateReportDto } from './dto/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
 
 @Controller('reports')
 export class ReportsController {
@@ -9,8 +11,8 @@ export class ReportsController {
 
     @Post()
     @UseGuards(AuthGuard)
-    createReport(@Body() body: CreateReportDto){
-        return this.reportService.create(body);
+    createReport(@CurrentUser() user: User, @Body() body: CreateReportDto){
+        return this.reportService.create(body, user);
 
     }
 }
