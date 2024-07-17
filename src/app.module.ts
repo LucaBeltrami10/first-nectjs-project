d740c82517c447a1d7b6a8a09260865ffb4054e9
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-07-17 15:33:22 +0200

    created first migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { dataSourceOptions } from '../db/data-source';
+import * as ormconfig from '../ormconfig'
 const cookieSession = require('cookie-session');
 
 @Module({
@@ -16,24 +18,12 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    // comunichiamo al sistema di gestione delle dipendenze che vogliomo andare a cercare il ConfigService
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService)=> {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          synchronize:true,
-          entities: [User, Report],
-        }
-      },
-    }),
-  //   TypeOrmModule.forRoot({
   //   type: 'sqlite',
   //   database: 'db.sqlite',
   //   entities: [User, Report],
   //   synchronize: true,
   // }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     UsersModule, 
     ReportsModule,
     ],
